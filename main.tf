d8d64906a48ef64e6db2bb218992cf0c7f99c2c8
akshithaacha <akshitha.acha@gmail.com>
2023-06-12 18:03:26 -0400

    add-terraform-files

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.region}"
+    access_key = "AKIAY3QUCCLPDL3FF5SV"
+    secret_key = "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"
+    region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "devopstest01" {
+    #source = "s3.tf"
+    #bucket name should be unique
+    bucket = "devops-test-bucket-9494"       
 }
